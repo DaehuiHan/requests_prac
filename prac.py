f691f558e152d5bd3105b1c3956c73ae5a53c63e
DaehuiHan <79817611+DaehuiHan@users.noreply.github.com>
2021-05-06 11:45:32 +0900

    가상환경 설정 및 prac.py

---
+++ prac.py
@@ -0,0 +1,16 @@
+import requests  # requests 라이브러리 설치 필요
+
+r = requests.get(
+    'http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+rjson = r.json()
+
+# print(rjson['RealtimeCityAir']['row'][0]['MSRSTE_NM'])
+
+gus = rjson['RealtimeCityAir']['row']
+
+for gu in gus:
+    gu_name = gu['MSRSTE_NM']
+    gu_mise = gu['IDEX_MVL']
+
+    if (gu_mise > 85):
+        print(gu_name, gu_mise)
